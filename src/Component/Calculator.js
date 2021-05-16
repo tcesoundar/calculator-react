65af2583e12ea226c19d02ec4c93dfc4acdd6008
T.Soundarrajan <tcesoundar@gmail.com>
2021-05-16 19:30:34 +0530

    [Soundar] Add. button enable disable option

--- src/Component/Calculator.js
+++ src/Component/Calculator.js
@@ -6,69 +6,74 @@ export default class Calculator extends Component {
         super(props);
 
         this.state = {
-            value1 : '', 
-            value2 : '',
-            res: ''
+            firstInput : '', 
+            secondInput : '',
+            result: ''
         }
 
-        this.handleValue1 = this.handleValue1.bind(this)
-        this.handleValue2 = this.handleValue2.bind(this)
+        this.handleFirstInput = this.handleFirstInput.bind(this)
+        this.handleSecondInput = this.handleSecondInput.bind(this)
 
         this.add = this.add.bind(this)
         this.sub = this.sub.bind(this)
         this.division = this.division.bind(this)
         this.multiply = this.multiply.bind(this)
-    
+
     }
 
-    handleValue1 = e => this.setState({
-        value1 : e.target.value
+    handleFirstInput = e => this.setState({
+        firstInput : e.target.value
     })
 
-    handleValue2 = e => this.setState({
-        value2 : e.target.value
+    handleSecondInput = e => this.setState({
+        secondInput : e.target.value
     })
 
     add =  e => this.setState({
-        res : parseInt(this.state.value1) + parseInt(this.state.value2)
+        result : parseInt(this.state.firstInput) + parseInt(this.state.secondInput)
     })
 
     sub =  e => this.setState({
-        res : parseInt(this.state.value1) - parseInt(this.state.value2)
+        result : parseInt(this.state.firstInput) - parseInt(this.state.secondInput)
     })
 
     division =  e => this.setState({
-        res : parseInt(this.state.value1) / parseInt(this.state.value2)
+        result : parseInt(this.state.firstInput) / parseInt(this.state.secondInput)
     })
 
     multiply =  e => this.setState({
-        res : parseInt(this.state.value1) * parseInt(this.state.value2)
+        result : parseInt(this.state.firstInput) * parseInt(this.state.secondInput)
     })
-
+    
     render() {
+
+        const {firstInput, secondInput, result} = this.state
+
+        const isDisabled = firstInput.length < 1 || secondInput.length < 1
+        
         return (
             <div>
 
                 <p><b>Calculator Application</b></p>
                 <label>First Input : </label>
-                <input name='firstInput' type='number' placeholder='0' value={this.state.value1} onChange={this.handleValue1}/>
+                <input name='firstInput' type='number' placeholder='0' value={firstInput} onChange={this.handleFirstInput}/>
                 <br/>
                 <br/>
 
                 <label>Second Input : </label>
-                <input name='secondInput' type='number' placeholder='0' value={this.state.value2} onChange={this.handleValue2}/>
+                <input name='secondInput' type='number' placeholder='0' value={secondInput} onChange={this.handleSecondInput}/>
                 <br/>
                 <br/>
 
-                <input name='add' type='button' value='+' onClick={this.add}/>
-                <input name='sub' type='button' value='-' onClick={this.sub}/>
-                <input name='div' type='button' value='/' onClick={this.division}/>
-                <input name='multiply' type='button' value='*' onClick={this.multiply}/>
+                <input name='add' type='button' value='+' onClick={this.add} disabled={isDisabled}/>
+                <input name='sub' type='button' value='-' onClick={this.sub} disabled={isDisabled}/>
+                <input name='division' type='button' value='/' onClick={this.division} disabled={isDisabled}/>
+                <input name='multiply' type='button' value='*' onClick={this.multiply} disabled={isDisabled}/>
                 <br/>
                 <br/>
 
                 <label>Result : </label>
-                <input name='result' type='number' value={this.state.res}/>
+                <input name='result' type='text' placeholder='0' value= {result}/>
 
             </div>
         )
